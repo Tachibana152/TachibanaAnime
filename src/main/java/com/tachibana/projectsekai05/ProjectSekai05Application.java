dcf2d8ffca72960b3d50b1511ea6778a36c2c764
Tachibana <a359938435@163.com>
2026-08-25 15:09:19 +0800

    Initial commit

---
+++ src/main/java/com/tachibana/projectsekai05/ProjectSekai05Application.java
@@ -0,0 +1,13 @@
+package com.tachibana.projectsekai05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSekai05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSekai05Application.class, args);
+    }
+
+}
